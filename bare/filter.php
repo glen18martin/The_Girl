e2c56ddfa0756e168c186905190c2d80040a8b99
Royston Lobo <roydl.net@gmail.com>
2017-10-15 16:42:41 +0530

    mitm

--- bare/filter.php
+++ bare/filter.php
@@ -3,5 +3,8 @@
 exec("~/mitmdump  -nr /var/www/html/The_Girl/tcore/mitmdump -w /var/www/html/The_Girl/filtered \"~m post\"");
 
 
-echo file_get_contents("/var/www/html/The_Girl/filtered");
+$data = file_get_contents("/var/www/html/The_Girl/filtered");
+
+preg_match('/indentifier=(.*)&password=(.*)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
+print_r($matches);
 ?>
